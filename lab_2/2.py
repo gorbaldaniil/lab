98c00624b63c3a505927c6c313d936a6f307a0df
Daniil <48162793+kuprisS@users.noreply.github.com>
2019-12-07 12:06:32 +0200

    add pingpong

--- lab_2/2.py
+++ lab_2/2.py
@@ -1,32 +1,27 @@
 def number_of_letters(user_string):
+    for letter in set(user_string):
+        if letter.isalpha():
+            x = user_string.count(letter)
+            print(letter, "=", x)
+
+
+def alfavit(user_string):
     result1 = []
-    i=0
-    b=""
-    for letter in user_string:
-        f = None
-        x = letter
-        for l in b:
-            if x == l or x == " ":
-                f = True
-        if f != True:
-            b += x
-            for l in user_string:
-                if x == l:
-                    i += 1
-            print(x,'=',i)
-            i = 0
-            f = None
-
     for word in user_string.split():
-        if word not in result1:
-            result1.append(word)
-
+        if len(word)>2 and word.isalpha():
+            if word not in result1:
+                result1.append(word)
     userWords = result1
     userWords.sort()
     result = " ".join(userWords)
-
     print(result)
 
 
 user_string = input("Enter: ")
-number_of_letters(user_string)
+
+enter = input("a or b")
+
+if enter == "a":
+    number_of_letters(user_string)
+elif enter == "b":
+    alfavit(user_string)
